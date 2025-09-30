5f6be19f7782f39ddb788eae40d0ada8c0ab87b2
Zargovic <echaouiseifeddine@gmailcom>
2025-09-30 10:00:55 +0100

    first one

---
+++ Backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT =5000
+
+
+
+app.get("/", (req, res) => {res.send("API is running...");});
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    });
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,5 @@
+import './styles/App.css'
+
+
+
+export default App
